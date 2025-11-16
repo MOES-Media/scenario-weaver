dc547c5eda650b63153f55347d39477be780bed2
MOES-Media <info@moes-media.be>
2025-11-16 01:08:09 +0100

    fix: fixes formatting errors

--- examples/src/main/java/be/moesmedia/scenarioweaver/examples/hellospring/HelloSpringApplication.java
+++ examples/src/main/java/be/moesmedia/scenarioweaver/examples/hellospring/HelloSpringApplication.java
@@ -23,8 +23,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloSpringApplication {
 
-    private HelloSpringApplication() {
-    }
+    private HelloSpringApplication() {}
 
     public static void main(String[] args) {
         SpringApplication.run(HelloSpringApplication.class, args);
